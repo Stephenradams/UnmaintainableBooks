07bf06c2803d8ecbe08b41ccc883f4e14084d0c7
Stephen Adams <stephen@cgcsoftware.co.uk>
2020-11-08 13:43:44 +0000

    Setup basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BooksPageComponent } from './books-page/books-page.component';
+import { BooksListComponent } from './books-list/books-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksPageComponent,
+    BooksListComponent
   ],
   imports: [
     BrowserModule,
